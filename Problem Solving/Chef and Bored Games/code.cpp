639d7f3805f5e97193bdcea3cac8736877f59598
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-07-20 00:16:30 +0530

    Create code.cpp

---
+++ Problem Solving/Chef and Bored Games/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int sum = 0;
+	    for(int i=1; i<=n; i++){
+	        if(i%2 != 0){
+	            sum += (n-i+1)*(n-i+1);
+	        }
+	    }
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
